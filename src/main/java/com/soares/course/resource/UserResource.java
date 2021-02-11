d18bc36210c6f15f01cd9c9e11a81ec6321fd32b
ediel2808 <danieleduardo1545@gmail.com>
2021-02-11 09:38:04 -0300

    User entity and resource

---
+++ src/main/java/com/soares/course/resource/UserResource.java
@@ -0,0 +1,21 @@
+package com.soares.course.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.soares.course.entites.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		
+		User u = new User(1L,"Ediel soares","daniduardo1545@gmail.com", "999999999","1234" );
+		
+		return ResponseEntity.ok().body(u);
+	}
+}
